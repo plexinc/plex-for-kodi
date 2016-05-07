ccc2dc385d475cab7f15ea914705bdf37b1e6b36
ruuk <xbmc@2ndmind.net>
2016-05-07 16:06:24 -0700

    Big refactor of hub filling code Finish adding all known hubs Hide hubs while filling to remove flicker

--- lib/windows/home.py
+++ lib/windows/home.py
@@ -78,32 +78,84 @@ class HomeWindow(kodigui.BaseWindow):
     HUB_POSTER_12 = 412
     HUB_POSTER_13 = 413
     HUB_POSTER_14 = 414
+    HUB_AR16X9_15 = 415
+    HUB_AR16X9_16 = 416
+    HUB_AR16X9_17 = 417
+
+    HUBMAP = {
+        # HOME
+        'home.continue': {'index': 0, 'with_progress': True, 'with_art': True},
+        'home.ondeck': {'index': 1},
+        'home.television.recent': {'index': 3},
+        'home.movies.recent': {'index': 4},
+        'home.music.recent': {'index': 5},
+        'home.videos.recent': {'index': 6},
+        # SHOW
+        'tv.ondeck': {'index': 1},
+        'tv.recentlyaired': {'index': 2},
+        'tv.recentlyadded': {'index': 3},
+        'tv.inprogress': {'index': 4, 'with_progress': True},
+        'tv.startwatching': {'index': 7},
+        'tv.rediscover': {'index': 8},
+        'tv.morefromnetwork': {'index': 11},
+        'tv.toprated': {'index': 12},
+        'tv.moreingenre': {'index': 13},
+        'tv.recentlyviewed': {'index': 14},
+        # MOVIE
+        'movie.inprogress': {'index': 0, 'with_progress': True, 'with_art': True},
+        'movie.recentlyreleased': {'index': 1},
+        'movie.recentlyadded': {'index': 2},
+        'movie.genre': {'index': 3},
+        'movie.director': {'index': 7},
+        'movie.actor': {'index': 8},
+        'movie.topunwatched': {'index': 11},
+        'movie.recentlyviewed': {'index': 12},
+        # ARTIST
+        'music.recent.added': {'index': 5},
+        'music.videos.popular.new': {'index': 6},
+        # PHOTO
+        'photo.recent': {'index': 5},
+        'photo.random.year': {'index': 9},
+        'photo.random.decade': {'index': 10},
+        # VIDEO
+        'video.recent': {'index': 0},
+        'video.random.year': {'index': 6},
+        'video.random.decade': {'index': 15},
+        'video.inprogress': {'index': 16, 'with_progress': True, 'with_art': True},
+        'video.unwatched.random': {'index': 17},
+    }
 
     def __init__(self, *args, **kwargs):
         kodigui.BaseWindow.__init__(self, *args, **kwargs)
         self.lastSection = HomeSection
         self.task = None
         self.closeOption = None
+        self.hubControls = None
         self.sectionHubs = {}
 
     def onFirstInit(self):
         self.sectionList = kodigui.ManagedControlList(self, self.SECTION_LIST_ID, 7)
 
-        self.hub_AR16x9_00 = kodigui.ManagedControlList(self, self.HUB_AR16X9_00, 5)
-        self.hub_POSTER_01 = kodigui.ManagedControlList(self, self.HUB_POSTER_01, 5)
-        self.hub_POSTER_02 = kodigui.ManagedControlList(self, self.HUB_POSTER_02, 5)
-        self.hub_POSTER_03 = kodigui.ManagedControlList(self, self.HUB_POSTER_03, 5)
-        self.hub_POSTER_04 = kodigui.ManagedControlList(self, self.HUB_POSTER_04, 5)
-        self.hub_SQUARE_05 = kodigui.ManagedControlList(self, self.HUB_SQUARE_05, 5)
-        self.hub_AR16x9_06 = kodigui.ManagedControlList(self, self.HUB_AR16X9_06, 5)
-        self.hub_POSTER_07 = kodigui.ManagedControlList(self, self.HUB_POSTER_07, 5)
-        self.hub_POSTER_08 = kodigui.ManagedControlList(self, self.HUB_POSTER_08, 5)
-        self.hub_SQUARE_09 = kodigui.ManagedControlList(self, self.HUB_SQUARE_09, 5)
-        self.hub_SQUARE_10 = kodigui.ManagedControlList(self, self.HUB_SQUARE_10, 5)
-        self.hub_POSTER_11 = kodigui.ManagedControlList(self, self.HUB_POSTER_11, 5)
-        self.hub_POSTER_12 = kodigui.ManagedControlList(self, self.HUB_POSTER_12, 5)
-        self.hub_POSTER_13 = kodigui.ManagedControlList(self, self.HUB_POSTER_13, 5)
-        self.hub_POSTER_14 = kodigui.ManagedControlList(self, self.HUB_POSTER_14, 5)
+        self.hubControls = (
+            kodigui.ManagedControlList(self, self.HUB_AR16X9_00, 5),
+            kodigui.ManagedControlList(self, self.HUB_POSTER_01, 5),
+            kodigui.ManagedControlList(self, self.HUB_POSTER_02, 5),
+            kodigui.ManagedControlList(self, self.HUB_POSTER_03, 5),
+            kodigui.ManagedControlList(self, self.HUB_POSTER_04, 5),
+            kodigui.ManagedControlList(self, self.HUB_SQUARE_05, 5),
+            kodigui.ManagedControlList(self, self.HUB_AR16X9_06, 5),
+            kodigui.ManagedControlList(self, self.HUB_POSTER_07, 5),
+            kodigui.ManagedControlList(self, self.HUB_POSTER_08, 5),
+            kodigui.ManagedControlList(self, self.HUB_SQUARE_09, 5),
+            kodigui.ManagedControlList(self, self.HUB_SQUARE_10, 5),
+            kodigui.ManagedControlList(self, self.HUB_POSTER_11, 5),
+            kodigui.ManagedControlList(self, self.HUB_POSTER_12, 5),
+            kodigui.ManagedControlList(self, self.HUB_POSTER_13, 5),
+            kodigui.ManagedControlList(self, self.HUB_POSTER_14, 5),
+            kodigui.ManagedControlList(self, self.HUB_AR16X9_15, 5),
+            kodigui.ManagedControlList(self, self.HUB_AR16X9_16, 5),
+            kodigui.ManagedControlList(self, self.HUB_AR16X9_17, 5)
+        )
 
         self.bottomItem = 0
         self.serverRefresh()
@@ -206,6 +258,13 @@ class HomeWindow(kodigui.BaseWindow):
         self.setFocusId(self.SECTION_LIST_ID)
 
     def showHubs(self, section=None):
+        self.setProperty('drawing', '1')
+        try:
+            self._showHubs(section=section)
+        finally:
+            self.setProperty('drawing', '')
+
+    def _showHubs(self, section=None):
         self.clearHubs()
 
         hubs = self.sectionHubs.get(section.key)
@@ -214,141 +273,56 @@ class HomeWindow(kodigui.BaseWindow):
                 self.task.moveUpSection(section)
             return
 
-        if section.type == 'show':
-            return self.showShowHubs(hubs)
-        elif section.type == 'artist':
-            return self.showMusicHubs(hubs)
-        elif section.type == 'photo':
-            return self.showPhotoHubs(hubs)
-
         for hub in hubs:
-            util.DEBUG_LOG('Hub: {0} ({1})'.format(hub.hubIdentifier, len(hub.items)))
-            if hub.hubIdentifier in ('home.continue', 'movie.inprogress'):
-                self.showHub_AR16x9_00(hub)
-            elif hub.hubIdentifier == 'home.ondeck':
-                self.showHub_POSTER_01(hub)
-            elif hub.hubIdentifier == 'home.television.recent':
-                self.showHub_POSTER_03(hub)
-            elif hub.hubIdentifier in ('home.movies.recent', 'movie.recentlyadded'):
-                self.showHub_POSTER_04(hub)
-            elif hub.hubIdentifier == 'home.music.recent':
-                self.showHub_SQUARE_05(hub)
-            elif hub.hubIdentifier in ('home.videos.recent', 'video.recent'):
-                self.showHub_AR16x9_06(hub)
+            if hub.hubIdentifier in self.HUBMAP:
+                util.DEBUG_LOG('Hub: {0} ({1})'.format(hub.hubIdentifier, len(hub.items)))
+                self.showHub(hub, **self.HUBMAP[hub.hubIdentifier])
+            else:
+                util.DEBUG_LOG('UNHANDLED - Hub: {0} ({1})'.format(hub.hubIdentifier, len(hub.items)))
 
-    def showShowHubs(self, hubs):
-        for hub in hubs:
-            util.DEBUG_LOG('Hub (Show): {0} ({1})'.format(hub.hubIdentifier, len(hub.items)))
-
-            if hub.hubIdentifier == 'tv.ondeck':
-                self.showHub_POSTER_01(hub)
-            elif hub.hubIdentifier == 'tv.recentlyaired':
-                self.showHub_POSTER_02(hub)
-            elif hub.hubIdentifier == 'tv.recentlyadded':
-                self.showHub_POSTER_03(hub)
-            elif hub.hubIdentifier == 'tv.inprogress':
-                self.showHub_POSTER_04(hub, with_progress=True)
-            elif hub.hubIdentifier == 'tv.startwatching':
-                self.showHub_POSTER_07(hub)
-            elif hub.hubIdentifier == 'tv.rediscover':
-                self.showHub_POSTER_08(hub)
-            elif hub.hubIdentifier == 'tv.morefromnetwork':
-                self.showHub_POSTER_11(hub)
-            elif hub.hubIdentifier == 'tv.toprated':
-                self.showHub_POSTER_12(hub)
-            elif hub.hubIdentifier == 'tv.moreingenre':
-                self.showHub_POSTER_13(hub)
-            elif hub.hubIdentifier == 'tv.recentlyviewed':
-                self.showHub_POSTER_14(hub)
-
-    def showMusicHubs(self, hubs):
-        for hub in hubs:
-            util.DEBUG_LOG('Hub (Music): {0} ({1})'.format(hub.hubIdentifier, len(hub.items)))
-
-            if hub.hubIdentifier == 'music.recent.added':
-                self.showHub_SQUARE_05(hub)
-            elif hub.hubIdentifier == 'music.videos.popular.new':
-                self.showHub_AR16x9_06(hub)
-
-    def showPhotoHubs(self, hubs):
-        for hub in hubs:
-            util.DEBUG_LOG('Hub (Photo): {0} ({1})'.format(hub.hubIdentifier, len(hub.items)))
-
-            if hub.hubIdentifier == 'photo.recent':
-                self.showHub_SQUARE_05(hub)
-            elif hub.hubIdentifier == 'photo.random.year':
-                self.showHub_SQUARE_09(hub)
-            elif hub.hubIdentifier == 'photo.random.decade':
-                self.showHub_SQUARE_10(hub)
-
-    def createEpisodeListItem(self, ep):
+    def createGrandparentedListItem(self, ep):
         mli = kodigui.ManagedListItem(ep.grandparentTitle, thumbnailImage=ep.thumbUrl, data_source=ep)
-        prog = util.getProgressImage(ep)
-        if prog:
-            mli.setProperty('progress', prog)
-
         return mli
 
-    def createSeasonListItem(self, season):
+    def createParentedListItem(self, season):
         mli = kodigui.ManagedListItem(season.parentTitle, thumbnailImage=season.thumbUrl, data_source=season)
         return mli
 
-    def createMovieListItem(self, movie):
-        mli = kodigui.ManagedListItem(movie.title, thumbnailImage=movie.thumbUrl, data_source=movie)
-        return mli
-
-    def createShowListItem(self, show):
-        mli = kodigui.ManagedListItem(show.title, thumbnailImage=show.thumbUrl, data_source=show)
-        return mli
-
-    def createAlbumListItem(self, album):
-        mli = kodigui.ManagedListItem(album.title, thumbnailImage=album.thumbUrl, data_source=album)
-        return mli
-
-    def createClipListItem(self, clip):
-        mli = kodigui.ManagedListItem(clip.title, thumbnailImage=clip.thumbUrl, data_source=clip)
+    def createSimpleListItem(self, obj):
+        mli = kodigui.ManagedListItem(obj.title, thumbnailImage=obj.thumbUrl, data_source=obj)
         return mli
 
     def createListItem(self, obj):
         if obj.type == 'episode':
-            return self.createEpisodeListItem(obj)
+            return self.createGrandparentedListItem(obj)
         elif obj.type == 'season':
-            return self.createSeasonListItem(obj)
+            return self.createParentedListItem(obj)
         elif obj.type == 'movie':
-            return self.createMovieListItem(obj)
+            return self.createSimpleListItem(obj)
         elif obj.type == 'show':
-            return self.createShowListItem(obj)
+            return self.createSimpleListItem(obj)
         elif obj.type == 'album':
-            return self.createSeasonListItem(obj)
+            return self.createParentedListItem(obj)
         elif obj.type == 'track':
-            return self.createSeasonListItem(obj)
+            return self.createParentedListItem(obj)
         elif obj.type == 'photo':
-            return self.createShowListItem(obj)
+            return self.createSimpleListItem(obj)
         elif obj.type == 'clip':
-            return self.createClipListItem(obj)
+            return self.createSimpleListItem(obj)
         else:
             util.DEBUG_LOG('Unhandled Hub item: {0}'.format(obj.type))
 
     def clearHubs(self):
-        self.hub_AR16x9_00.reset()
-        self.hub_POSTER_01.reset()
-        self.hub_POSTER_02.reset()
-        self.hub_POSTER_03.reset()
-        self.hub_POSTER_04.reset()
-        self.hub_SQUARE_05.reset()
-        self.hub_AR16x9_06.reset()
-        self.hub_POSTER_07.reset()
-        self.hub_POSTER_08.reset()
-        self.hub_SQUARE_09.reset()
-        self.hub_SQUARE_10.reset()
-        self.hub_POSTER_11.reset()
-        self.hub_POSTER_12.reset()
-        self.hub_POSTER_13.reset()
-        self.hub_POSTER_14.reset()
+        for control in self.hubControls:
+            control.reset()
 
-    def showHub(self, control, hub, with_progress=False):
+    def showHub(self, hub, index=None, with_progress=False, with_art=False):
         if not hub.items:
             return
+
+        self.setProperty('hub.4{0:02d}'.format(index), hub.title)
+
+        control = self.hubControls[index]
 
         items = []
         for obj in hub.items:
@@ -356,79 +330,11 @@ class HomeWindow(kodigui.BaseWindow):
             if mli:
                 if with_progress:
                     mli.setProperty('progress', util.getProgressImage(obj))
+                if with_art:
+                    mli.setThumbnailImage(obj.artUrl)
                 items.append(mli)
 
         control.addItems(items)
-
-    def showHub_AR16x9_00(self, hub):
-        if not hub.items:
-            return
-
-        self.setProperty('hub.400', hub.title)
-
-        mitems = []
-        for hitem in hub.items:
-            mli = kodigui.ManagedListItem(hitem.title, thumbnailImage=hitem.artUrl, data_source=hitem)
-            mli.setProperty('progress', util.getProgressImage(hitem))
-            mitems.append(mli)
-
-        self.hub_AR16x9_00.addItems(mitems)
-
-    def showHub_POSTER_01(self, hub, with_progress=False):
-        self.setProperty('hub.401', hub.title)
-        return self.showHub(self.hub_POSTER_01, hub, with_progress)
-
-    def showHub_POSTER_02(self, hub, with_progress=False):
-        self.setProperty('hub.402', hub.title)
-        return self.showHub(self.hub_POSTER_02, hub, with_progress)
-
-    def showHub_POSTER_03(self, hub, with_progress=False):
-        self.setProperty('hub.403', hub.title)
-        return self.showHub(self.hub_POSTER_03, hub, with_progress)
-
-    def showHub_POSTER_04(self, hub, with_progress=False):
-        self.setProperty('hub.404', hub.title)
-        return self.showHub(self.hub_POSTER_04, hub, with_progress)
-
-    def showHub_SQUARE_05(self, hub):
-        self.setProperty('hub.405', hub.title)
-        return self.showHub(self.hub_SQUARE_05, hub)
-
-    def showHub_AR16x9_06(self, hub):
-        self.setProperty('hub.406', hub.title)
-        return self.showHub(self.hub_AR16x9_06, hub)
-
-    def showHub_POSTER_07(self, hub, with_progress=False):
-        self.setProperty('hub.407', hub.title)
-        return self.showHub(self.hub_POSTER_07, hub, with_progress)
-
-    def showHub_POSTER_08(self, hub, with_progress=False):
-        self.setProperty('hub.408', hub.title)
-        return self.showHub(self.hub_POSTER_08, hub, with_progress)
-
-    def showHub_SQUARE_09(self, hub):
-        self.setProperty('hub.409', hub.title)
-        self.showHub(self.hub_SQUARE_09, hub)
-
-    def showHub_SQUARE_10(self, hub):
-        self.setProperty('hub.410', hub.title)
-        self.showHub(self.hub_SQUARE_10, hub)
-
-    def showHub_POSTER_11(self, hub, with_progress=False):
-        self.setProperty('hub.411', hub.title)
-        return self.showHub(self.hub_POSTER_11, hub, with_progress)
-
-    def showHub_POSTER_12(self, hub, with_progress=False):
-        self.setProperty('hub.412', hub.title)
-        return self.showHub(self.hub_POSTER_12, hub, with_progress)
-
-    def showHub_POSTER_13(self, hub, with_progress=False):
-        self.setProperty('hub.413', hub.title)
-        return self.showHub(self.hub_POSTER_13, hub, with_progress)
-
-    def showHub_POSTER_14(self, hub, with_progress=False):
-        self.setProperty('hub.414', hub.title)
-        return self.showHub(self.hub_POSTER_14, hub, with_progress)
 
     def sectionClicked(self):
         item = self.sectionList.getSelectedItem()

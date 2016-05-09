2ac17ebe2eec057be793393b81cfd17ede523ec4
ruuk <xbmc@2ndmind.net>
2016-05-09 10:43:34 -0700

    Finish up show library view window navigation

--- lib/windows/shows.py
+++ lib/windows/shows.py
@@ -11,6 +11,10 @@ class ShowsWindow(kodigui.BaseWindow):
     width = 1920
     height = 1080
 
+    THUMB_POSTER_DIM = (287, 425)
+    THUMB_AR16X9_DIM = (619, 348)
+    THUMB_SQUARE_DIM = (425, 425)
+
     SHOW_PANEL_ID = 101
 
     def __init__(self, *args, **kwargs):
@@ -21,43 +25,64 @@ class ShowsWindow(kodigui.BaseWindow):
         self.showPanelControl = kodigui.ManagedControlList(self, self.SHOW_PANEL_ID, 5)
         self.fillShows()
 
-    def createGrandparentedListItem(self, obj):
+    def createGrandparentedListItem(self, obj, thumb_w, thumb_h):
         title = obj.grandparentTitle or obj.parentTitle or obj.title or ''
-        mli = kodigui.ManagedListItem(title, thumbnailImage=obj.thumbUrl, data_source=obj)
+        mli = kodigui.ManagedListItem(title, thumbnailImage=obj.transcodedThumbURL(thumb_w, thumb_h), data_source=obj)
         return mli
 
-    def createParentedListItem(self, obj):
+    def createParentedListItem(self, obj, thumb_w, thumb_h):
         title = obj.parentTitle or obj.title or ''
-        mli = kodigui.ManagedListItem(title, thumbnailImage=obj.thumbUrl, data_source=obj)
+        mli = kodigui.ManagedListItem(title, thumbnailImage=obj.transcodedThumbURL(thumb_w, thumb_h), data_source=obj)
         return mli
 
-    def createSimpleListItem(self, obj):
-        mli = kodigui.ManagedListItem(obj.title or '', thumbnailImage=obj.thumbUrl, data_source=obj)
+    def createSimpleListItem(self, obj, thumb_w, thumb_h):
+        mli = kodigui.ManagedListItem(obj.title or '', thumbnailImage=obj.transcodedThumbURL(thumb_w, thumb_h), data_source=obj)
         return mli
 
     def createListItem(self, obj):
         if obj.type == 'episode':
-            return self.createGrandparentedListItem(obj)
+            mli = self.createGrandparentedListItem(obj, *self.THUMB_POSTER_DIM)
+            mli.setProperty('thumb.fallback', 'script.plex/thumb_fallbacks/show.png')
+            return mli
         elif obj.type == 'season':
-            return self.createParentedListItem(obj)
+            mli = self.createParentedListItem(obj, *self.THUMB_POSTER_DIM)
+            mli.setProperty('thumb.fallback', 'script.plex/thumb_fallbacks/show.png')
+            return mli
         elif obj.type == 'movie':
-            return self.createSimpleListItem(obj)
+            mli = self.createSimpleListItem(obj, *self.THUMB_POSTER_DIM)
+            mli.setProperty('thumb.fallback', 'script.plex/thumb_fallbacks/movie.png')
+            return mli
         elif obj.type == 'show':
-            return self.createSimpleListItem(obj)
+            mli = self.createSimpleListItem(obj, *self.THUMB_POSTER_DIM)
+            mli.setProperty('thumb.fallback', 'script.plex/thumb_fallbacks/show.png')
+            return mli
         elif obj.type == 'album':
-            return self.createParentedListItem(obj)
+            mli = self.createParentedListItem(obj, *self.THUMB_SQUARE_DIM)
+            mli.setProperty('thumb.fallback', 'script.plex/thumb_fallbacks/music.png')
+            return mli
         elif obj.type == 'track':
-            return self.createParentedListItem(obj)
+            mli = self.createParentedListItem(obj, *self.THUMB_SQUARE_DIM)
+            mli.setProperty('thumb.fallback', 'script.plex/thumb_fallbacks/music.png')
+            return mli
         elif obj.type == 'photo':
-            return self.createSimpleListItem(obj)
+            mli = self.createSimpleListItem(obj, *self.THUMB_SQUARE_DIM)
+            mli.setProperty('thumb.fallback', 'script.plex/thumb_fallbacks/photo.png')
+            return mli
         elif obj.type == 'clip':
-            return self.createSimpleListItem(obj)
+            mli = self.createSimpleListItem(obj, *self.THUMB_AR16X9_DIM)
+            mli.setProperty('thumb.fallback', 'script.plex/thumb_fallbacks/movie16x9.png')
+            return mli
+        else:
+            util.DEBUG_LOG('Unhandled Hub item: {0}'.format(obj.type))
 
     def fillShows(self):
         items = []
+        idx = 0
         for show in self.section.all():
             mli = self.createListItem(show)
             if mli:
+                mli.setProperty('index', str(idx))
                 items.append(mli)
+                idx += 1
 
         self.showPanelControl.addItems(items)

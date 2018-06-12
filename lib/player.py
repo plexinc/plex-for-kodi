6ae29538e3760b7efb0c59755d81180ad641d1c7
panni <panni@fragstore.net>
2018-06-12 14:14:30 +0200

    BGMPlayerHandler: add debug logging; don't show notification on playback failed

--- lib/player.py
+++ lib/player.py
@@ -633,6 +633,7 @@ class BGMPlayerHandler(BasePlayerHandler):
         self.oldVolume = util.rpc.Application.GetProperties(properties=["volume"])["volume"]
 
     def onPlayBackStarted(self):
+        util.DEBUG_LOG("BGM: playing theme for %s" % self.currentlyPlaying)
         self.player.bgmPlaying = True
 
     def setVolume(self, volume=None, reset=False):
@@ -644,6 +645,7 @@ class BGMPlayerHandler(BasePlayerHandler):
         self.setVolume(reset=True)
 
     def onPlayBackStopped(self):
+        util.DEBUG_LOG("BGM: stopped theme for %s" % self.currentlyPlaying)
         util.setGlobalProperty('theme_playing', '')
         self.player.bgmPlaying = False
         self.resetVolume()
@@ -653,7 +655,6 @@ class BGMPlayerHandler(BasePlayerHandler):
 
     def onPlayBackFailed(self):
         self.onPlayBackStopped()
-        return True
 
     def close(self):
         self.player.stopAndWait()

3cfe23eecf0eb696ec7e2bf76f0c452a6aed8d2e
panni <panni@fragstore.net>
2018-08-15 16:24:59 +0200

    go home faster in scrolled home section views

--- lib/windows/home.py
+++ lib/windows/home.py
@@ -460,10 +460,11 @@ class HomeWindow(kodigui.BaseWindow, util.CronReceiver):
 
                 if util.advancedSettings.fastBack and action != xbmcgui.ACTION_CONTEXT_MENU and \
                         self.lastSection != HomeSection and self.lastFocusID != self.SECTION_LIST_ID:
+                    self.setProperty('hub.focus', '0')
                     self.sectionList.selectItem(0)
                     self.lastSection = HomeSection
-                    self.sectionChanged(True)
                     self.setFocusId(self.SECTION_LIST_ID)
+                    self._sectionReallyChanged()
                     return
 
                 if not self.confirmExit():
